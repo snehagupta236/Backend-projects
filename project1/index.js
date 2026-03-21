0dc34885fd1f938fa1a2a54d36ac97083ae7354e
sneha <sneha0122345@gmail.com>
2026-03-21 11:53:13 +0530

    first backend project

---
+++ project1/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const Path = require('path');
+const fs = require('fs');
+const { log } = require('console');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('view engine' , 'ejs');
+app.use(express.static(Path.join(__dirname, 'public')));
+
+
+app.get('/', (req,res)=>{
+  fs.readdir(`./files`, function(err,files){
+     res.render('index',{files:files});
+    
+  })
+ 
+})
+app.get('/file/:filename', (req,res)=>{
+  fs.readFile(`./files/${req.params.filename}` ,"utf-8",function(err, filedata){
+    res.render('show' , {filename: req.params.filename, filedata: filedata});
+    
+  })
+ 
+})
+app.get('/edit/:filename', (req,res)=>{
+  
+  res.render('edit', {filename: req.params.filename});
+})
+app.post('/edit', (req,res)=>{
+  
+  fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+    res.redirect("/");
+  })
+})
+app.post('/create', (req,res)=>{
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+    res.redirect("/");
+
+  })
+
+  
+ 
+})
+
+
+app.listen(3000, ()=>{
+  console.log("server is running");
+  
+})
