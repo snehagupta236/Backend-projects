499ca5912b5c0f63c606073fb691258223542f0d
sneha <sneha0122345@gmail.com>
2026-04-05 14:56:03 +0530

    mongodb project

---
+++ project3/models/post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/miniproject");
+const postSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+   date: {
+    type: Date,
+    default: Date.now
+   },
+   content: String,
+   likes: [
+    {type: mongoose.Schema.Types.ObjectId, ref: "user"}
+   ]
+});
+
+module.exports = mongoose.model('post' , postSchema);
